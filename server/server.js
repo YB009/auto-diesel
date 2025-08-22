d89b571723b70248e1692ce988a4beb2178eda61
YB009 <owohdaniel09@gmail.com>
2025-08-22 17:55:25 +0100

    NEW COMMIT

--- server/server.js
+++ server/server.js
@@ -145,4 +145,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
+
+
+
 app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
